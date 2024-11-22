4da37ce145f34a411e52524895ac75fec8249ee8
MishelKV <mishelkvGH@gmail.com>
2024-11-22 17:49:18 +0300

    Modul2

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,14 +1,15 @@
 result = []
 insert1 = int(input("Введите число от 3 до 20 "))
-numbers1 = 0
-while numbers1 < 21:
-    numbers1+=1
-    numbers2 = 1
-    while numbers2 < 21:
-        if insert1 % (numbers1+numbers2) == 0 and numbers1<numbers2:
+numbers1 = 1
+while numbers1 < insert1:
+    numbers2 = numbers1+1
+    while numbers2 < insert1:
+        if insert1 % (numbers1+numbers2) == 0:
             result.append(numbers1)
             result.append(numbers2)
         numbers2 += 1
+    numbers1 += 1
+
 result0= ' '
 for i in result:
     result0 = result0 + str(i)
